48eaebb7a7600a384b2af1ffb27fec436b8fbf62
kgbajabiamila0 <kgbajabiamila0@CC270-86272.frostburg.edu>
2018-09-09 19:21:57 -0400

    This is manageable

---
+++ COSC310_ASN/src/cosc310_test/GbajabiamilaKashif_Test00.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.cosc310_test;
+
+import src.edu.frostburg.cosc310.lab00.GbajabiamilaKashifLab00;
+
+/**
+ *
+ * @author kgbajabiamila0
+ */
+public class GbajabiamilaKashif_Test00 {
+    
+    public static void main(String[] args){
+        GbajabiamilaKashifLab00 lab = new GbajabiamilaKashifLab00();
+    System.out.println(lab.problem0(9999));
+    System.out.println(lab.problem1(9999));
+    }
+}
