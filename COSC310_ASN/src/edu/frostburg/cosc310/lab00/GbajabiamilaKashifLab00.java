0323793f49a54d29046142774dd8bb014249c9cf
kgbajabiamila0 <kgbajabiamila0@CC270-86272.frostburg.edu>
2018-09-09 19:01:48 -0400

    This is annoying

---
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/GbajabiamilaKashifLab00.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src.edu.frostburg.cosc310.lab00;
+
+/**
+ *
+ * @author kgbajabiamila0
+ */
+public class GbajabiamilaKashifLab00 {
+    int i = 0;
+}
